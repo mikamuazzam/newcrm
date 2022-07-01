6f5a6fd995f08427156f2c46ff0f5a76ab70785b
nenden <ndensme@gmail.com>
2022-07-01 15:07:04 +0700

    d

--- application/resources/views/layout/header.blade.php
+++ application/resources/views/layout/header.blade.php
@@ -1,4 +1,5 @@
 <head>
+    <?php error_reporting(E_ALL); ini_set('display_errors', 1);?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" id="meta-csrf" />
